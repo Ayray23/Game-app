da9bf873f88b2b57c47a0b2ebc483ab967aecc89
Ayray23 <rachealadebisi23@gmail.com>
2025-05-25 14:47:39 -0700

    Added the backend url

--- src/pages/tictactoe.jsx
+++ src/pages/tictactoe.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'; 
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3000');
+const socket = io('https://game-server-5q0s.onrender.com/');
 
 const Tictactoe = () => { 
   const [board, setBoard] = useState(Array(9).fill(null)); 
